5de1e6d43b0c622c735232a616b60949a5472f53
Matt Champagne <mmchamp95@gmail.com>
2024-10-29 23:48:44 -0400

    fix iterator types

---
+++ iterator.d.ts
@@ -0,0 +1,21 @@
+declare module "gleam/iterator.mjs" {
+  import type { CustomType } from "prelude";
+  export * from "gleam/iterator.mjs";
+
+  export class Stop extends CustomType {}
+
+  export class Continue<T> extends CustomType {
+    constructor(element: T, continuation: () => Action$<T>);
+
+    0: T;
+    1: () => Action$<T>;
+  }
+
+  export type Action$<T> = Stop | Continue<T>;
+
+  export class Iterator<T> extends CustomType {
+    constructor(continuation: () => Action$<T>);
+
+    continuation: () => Action$<T>;
+  }
+}
