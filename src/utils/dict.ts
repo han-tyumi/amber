b971b7aabbcc4621cc129cbce2244ee428f6aaec
Matt Champagne <mmchamp95@gmail.com>
2024-12-30 14:45:39 -0500

    improve dict typings

--- src/utils/dict.ts
+++ src/utils/dict.ts
@@ -3,7 +3,7 @@ import * as dict$ from "$/gleam_stdlib/gleam/dict.mjs";
 export function toObjectWithMap<K extends string | number | symbol, V, VV>(
   dict: dict$.Dict$<K, V>,
   map: (value: V) => VV,
-) {
+): Record<K, VV> {
   const acc = {} as Record<K, VV>;
   dict$.each(dict, (key: K, value: V) => {
     acc[key] = map(value);
@@ -13,6 +13,6 @@ export function toObjectWithMap<K extends string | number | symbol, V, VV>(
 
 export function toObject<K extends string | number | symbol, V>(
   dict: dict$.Dict$<K, V>,
-) {
-  return toObjectWithMap(dict, (value) => value);
+): Record<K, V> {
+  return toObjectWithMap(dict, (value: V) => value);
 }
